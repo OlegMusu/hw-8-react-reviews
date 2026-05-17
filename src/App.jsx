4971173dd086c6a13c3eae7f16f9d56cb0651aa5
OlegMusu <175102757+OlegMusu@users.noreply.github.com>
2026-05-17 11:50:29 +0300

    hw-8

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import Statistics from './Components/Statistics/Statistics'
+import FeedbackOptions from './Components/FeedbackOptions/FeedbackOptions'
+import Section from './Components/Section/Section'
+import { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+  countTotalFeedback = () => {
+    const {good, neutral, bad} = this.state
+    return good + neutral + bad
+  }
+
+  handleCount = (type) => {
+    this.setState((prev) => ({
+      [type]: prev[type] + 1
+    }))
+  }
+
+  countPositiveFeedbackPercentage = () => {
+    const {good} = this.state
+    return (
+      Math.floor((good / this.countTotalFeedback()) * 100)
+    )
+  }
+
+  render() {
+
+    const {good, neutral, bad} = this.state
+
+    const option = Object.keys(this.state)
+
+    return (
+      <>
+      <Section title="please leave feedback">
+        <FeedbackOptions options={option} onLeaveFeedback={this.handleCount}/>
+      </Section>
+      <Section title="Statistics">
+        {this.countPositiveFeedbackPercentage()>0? <Statistics good={good} neutral={neutral} bad={bad} total={this.countTotalFeedback()} positivePercentage={this.countPositiveFeedbackPercentage()}/> : "There is no feedback"}
+      </Section>
+      </>
+    )
+  } 
+}
+
+export default App
